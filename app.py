4a9cfe7d0bf6641a7fc7f6f4a85555bb9743ccf2
Joseph Finlayson <joseph.finlayson@gmail.com>
2021-02-12 14:15:49 +0000

    add import

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ from picamera import PiCamera
 from time import sleep
 import io
 import glob
+import os
 
 def is_golden_hour():
     return requests.get("https://golden-hour.hobby-paas.cf/").json()['golden_hour']
 
-
 def process_video(images):
     print("sending images")
     file_data = [('image', ('long.jpg', image, 'image/jpeg')) for image in images]
-    # file_data = [('image', ('long.jpg', open('./long.jpg'), 'image/jpeg')), ('image', ('long.jpg', open('./long.jpg'), 'image/jpeg'))]
-    response = requests.post("https://golden-hour.hobby-paas.cf/post-story", files=file_data, data=dict(text="test1"))
+    response = requests.post("https://golden-hour.hobby-paas.cf/post-story", files=file_data, data=dict(text="Garden"))
     print(response)
     response.raise_for_status()
     with open("project.mp4", "wb") as f:
@@ -38,7 +37,7 @@ def post_to_instagram():
 
 def app():
     while True:
-
+        print("checking if golden hour")
         if is_golden_hour(): 
             camera = PiCamera()
             camera.resolution = (2592, 1944)
@@ -46,15 +45,13 @@ def app():
             for filename in camera.capture_continuous('img{counter:03d}.jpg'):
                 print('Captured %s' % filename)
                 if not is_golden_hour():
-                    sleep(5)
+                    sleep(5) 
                     create_video()
                     post_to_instagram()
                     delete_files()
                     camera.close()
                 sleep(300)
-
-            
-
+        sleep(60)
 
 if __name__ == "__main__":
     app()
